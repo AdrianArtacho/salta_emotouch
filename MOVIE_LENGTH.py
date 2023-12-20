fc64a91051ad4834d41cbf4e8abd1e3b0bac0df8
AdrianArtacho <adrian.artacho@gmail.com>
2023-12-20 19:07:09 +0100

    ...increment (WinOS)

--- MOVIE_LENGTH.py
+++ MOVIE_LENGTH.py
@@ -1,6 +1,6 @@
 import gui_abstractions.gui_choosefile as gui_choosefile
 
-filepath = gui_choosefile.main(['Select movie to find out length from', '','.mov'])
+filepath = gui_choosefile.main(['Select movie to find out length from', '',(".mov", ".mp4")])
 print(filepath)
 
 import pyt_abstractions.media.get_length as get_length
